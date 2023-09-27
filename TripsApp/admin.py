1c21a2f0991b8179f6e3cde626ccfd55096a8a46
Vinicius Cavalieri <125322132+ViniBOYs@users.noreply.github.com>
2023-09-26 22:46:21 -0300

    Requirements.txt Adicionado

--- TripsApp/admin.py
+++ TripsApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class detCategoria(admin.ModelAdmin):
+    list_display = ('id','nomeCategoria')
+    list_display_links = ('id','nomeCategoria')
+    search_fields= ('nomeCategoria')
+    list_per_page = ('0')
+admin.site.register(Categoria,detCategoria)
